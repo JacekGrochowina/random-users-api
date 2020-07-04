e16a133d23a77a53bba22fd726de352a719e91ab
Jacek Grochowina <jacek99@vp.pl>
2020-07-04 09:55:18 +0200

    create main template with global style and theme provider

---
+++ src/templates/MainTemplate.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { mainTheme } from 'theme/mainTheme';
+import GlobalStyle from 'theme/GlobalStyle';
+import storage from 'local-storage-fallback';
+
+function getInitialTheme() {
+    const savedTheme = storage.getItem('theme');
+    return savedTheme === null ? JSON.parse(savedTheme) : { ...mainTheme, mode: 'light' }
+}
+
+const MainTemplate = ({ children }) => {
+    const [theme, setTheme] = useState(getInitialTheme);
+    useEffect(() => {
+        storage.setItem('theme', JSON.stringify(theme))
+    }, [theme])
+
+    return (
+        <>
+            <ThemeProvider theme={theme}>
+                <GlobalStyle />
+                {children}
+                
+                {/* Only for testing - TO REMOVE LATER */}
+                {/* <button
+                    onClick={(e) => setTheme(
+                        theme.mode === 'dark' ? 
+                        { ...mainTheme, mode: 'light' } :
+                        { ...mainTheme, mode: 'dark' }
+                    )}
+                >
+                    Toggle Theme
+                </button> */}
+            </ThemeProvider>
+        </>
+    )
+}
+
+export default MainTemplate;
