6ffbad184b76d09d892c0a8c741fe63f366760f9
Jacek Grochowina <jacek99@vp.pl>
2020-07-05 22:44:25 +0200

    create Login Page

---
+++ src/views/LoginPage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import AuthTemplate from 'templates/AuthTemplate';
+import Heading from 'components/atoms/Heading/Heading';
+import Tone from 'components/atoms/Tone/Tone';
+import AuthIcons from 'components/molecules/AuthIcons/AuthIcons';
+
+const SyledHeading = styled(Heading)`
+    margin-top: 40px;
+    margin-left: 80px;
+    font-weight: ${({ theme }) => theme.fontWeight.semiBold};
+`;
+
+const LoginPage = () => {
+    return (
+        <AuthTemplate>
+            <Heading>Random Users <Tone>API</Tone></Heading>
+            <SyledHeading secondary>Zaloguj się</SyledHeading>
+            <AuthIcons />
+        </AuthTemplate>
+    )
+}
+
+export default LoginPage;
