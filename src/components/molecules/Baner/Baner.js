9f2f2ee079219c43250c06c8269217c7791114b6
Jacek Grochowina <jacek99@vp.pl>
2020-07-09 12:34:23 +0200

    create Baner component

---
+++ src/components/molecules/Baner/Baner.js
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+import Heading from 'components/atoms/Heading/Heading';
+import Tone from 'components/atoms/Tone/Tone';
+import User from 'components/atoms/User/User';
+import AuthUser from 'components/molecules/AuthUser/AuthUser';
+
+const Wrapper = styled.div`
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 40vh;
+    background-image: url(${({ theme }) => theme.mode === 'dark' ? theme.darkMode.bgImg : theme.lightMode.bgImg});
+`;
+
+const Container = styled.div`
+    padding: 15px;
+    width: 100%;
+    max-width: 900px;
+    margin: auto;
+`;
+
+const StyledHeading = styled(Heading)`
+    margin-bottom: 10vh;
+`;
+
+const Sidebar = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 300px;
+    height: 100vh;
+    padding: 10vh 20px;
+    transform: ${({ active }) => active ? 'translateX(0)' : 'translateX(-100%)'};
+    transition-property: transform;
+    transition-duration: 0.25s;
+    box-shadow: 5px 0 10px rgba(0, 0, 0, 0.25);
+    background-color: ${({ theme }) => theme.mode === 'dark' ? 
+      theme.color.dark : 
+      theme.color.light
+    };
+    z-index: 99;
+`;
+
+const Overlay = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background-color: rgba(0,0,0,0.7);
+    transform: ${({ active }) => active ? 'scale(1)' : 'scale(0)'};
+    opacity: ${({ active }) => active ? '1' : '0'};
+    transition-property: opacity;
+    transition-duration: 0.25s;
+    z-index: 98;
+`;
+
+const Baner = () => {
+    const [active, setActive] = useState(false);
+
+    return (
+        <Wrapper>
+            <User
+                onClick={() => setActive(!active)}
+                img="https://randomuser.me/api/portraits/men/41.jpg"
+            >
+                Milton <Tone>Ross</Tone>
+            </User>
+            <Container>
+                <StyledHeading>Random Users <Tone>API</Tone></StyledHeading>
+            </Container>
+
+            <Overlay active={active} onClick={() => setActive(!active)}/>
+            <Sidebar active={active}>
+                <AuthUser src="https://randomuser.me/api/portraits/men/41.jpg">Milton<br/> Ross</AuthUser>
+            </Sidebar>
+        </Wrapper>
+    )
+}
+
+export default Baner;
---
+++ src/components/molecules/Baner/Baner.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Baner from './Baner';
+
+storiesOf('Molecules/Baner', module)
+    .add('Normal', () => <Baner/>)
