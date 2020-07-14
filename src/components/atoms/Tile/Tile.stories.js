c3ccf4acd11f5eb83a144486a28dea1735b7160e
Jacek Grochowina <jacek99@vp.pl>
2020-07-14 10:32:45 +0200

    remove

--- src/components/atoms/Tile/Tile.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import Tile from './Tile';
-
-import UserPlus from 'assets/icons/user-plus-solid.svg';
-
-storiesOf('Atoms/Tile', module)
-    .add('Normal', () => <Tile icon={UserPlus}>Dodaj</Tile>)
