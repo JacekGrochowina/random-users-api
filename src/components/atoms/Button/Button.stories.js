6af87c3488d30f4f3a05d7b508aa5b2caaa9ab53
Jacek Grochowina <jacek99@vp.pl>
2020-07-14 10:30:11 +0200

    remove

--- src/components/atoms/Button/Button.stories.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import Button from './Button';
-
-storiesOf('Atoms/Button', module)
-    .add('Normal', () => <Button>Zarządzaj użytkownikami</Button>)
